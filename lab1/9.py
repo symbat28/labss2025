4d87cffaff481ca17fdb624edc47ec24bc2e4e33
symbat_d <s_duman@kbtu.kz>
2025-02-04 23:32:44 +0500

    python strings

--- lab1/9.py
+++ lab1/9.py
@@ -33,3 +33,43 @@ if "free" in txt:
   txt = "The best things in life are free!"
 if "expensive" not in txt:
   print("No, 'expensive' is NOT present.")
+
+
+
+  b = "Hello, World!"
+print(b[2:5])
+
+
+
+b = "Hello, World!"
+print(b[:5])
+
+
+b = "Hello, World!"
+print(b[2:])
+
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+
+a = "Hello, World!"
+print(a.upper())
+
+
+a="Hello,World!"
+print(a.lower())
+
+
+
+a = " Hello, World! "
+print(a.strip()) 
+
+
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+
+a = "Hello, World!"
+print(a.split(",")) 
