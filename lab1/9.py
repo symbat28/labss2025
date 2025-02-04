4e516dfe4819b8cfce9b6101ec77d6ff57bfd945
symbat_d <s_duman@kbtu.kz>
2025-02-04 23:21:25 +0500

    python strings

---
+++ lab1/9.py
@@ -0,0 +1,35 @@
+print("Hello")
+print('Hello')
+
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+a = "Hello, World!"
+print(a[1])
+
+
+for x in "banana":
+  print(x)
+
+
+a = "Hello, World!"
+print(len(a))
+
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+
+
+  txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
